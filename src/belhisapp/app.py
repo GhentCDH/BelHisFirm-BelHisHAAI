f3c1bba56ceb8fb9c1aee6b908d41d915ec35f8d
sanderdiericx <sanderdiericx@gmail.com>
2026-03-09 15:01:49 +0100

    refactor: ui structure

---
+++ src/belhisapp/app.py
@@ -0,0 +1,23 @@
+from textual.app import App, ComposeResult
+
+from src.belhisapp.widgets import FooterOption
+from src.belhisapp.widgets import FooterWidget
+from src.belhisapp.widgets import Logo
+
+from src.belhisapp.constants import AppConstants
+
+class BelhisApp(App):
+
+    CSS = AppConstants.CSS
+
+    def compose(self) -> ComposeResult:
+
+        yield Logo(AppConstants.LOGO)
+        yield Logo(AppConstants.INFO)
+        yield FooterWidget(["utils", "config", "quit"])
+
+    def on_footer_option_selected(self, message: FooterOption.Selected) -> None:
+        if message.option == "quit":
+            self.exit()
+        else:
+            self.notify(f"Selected: {message.option}")
---
+++ src/belhisapp/widgets/footer_option.py
@@ -0,0 +1,17 @@
+from textual.widgets import Static
+from textual.message import Message
+
+class FooterOption(Static):
+
+    class Selected(Message):
+        """Message sent when an option is selected."""
+        def __init__(self, option: str) -> None:
+            self.option = option
+            super().__init__()
+
+    def __init__(self, label: str, **kwargs) -> None:
+        super().__init__(label, **kwargs)
+        self.label = label
+
+    def on_click(self) -> None:
+        self.post_message(self.Selected(self.label))
