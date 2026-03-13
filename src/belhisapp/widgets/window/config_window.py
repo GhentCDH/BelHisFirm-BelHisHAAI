ea8749aa330dfd86f5dd0593a313cefd0066d25c
sanderdiericx <sanderdiericx@gmail.com>
2026-03-13 17:06:56 +0100

    feat: add reset button

--- src/belhisapp/widgets/window/config_window.py
+++ src/belhisapp/widgets/window/config_window.py
@@ -1,4 +1,4 @@
-from textual.containers import Center
+from textual.containers import Center, Horizontal, Vertical
 from textual.widget import Widget
 from textual.widgets import Static, Button
 
@@ -10,10 +10,14 @@ from src.belhisapp.config.build_form_result import BuildFormResult
 
 class ConfigWindow(Window):
 
+    _header: Static
+
     _form: Center
     _config_inputs: list[ConfigInput]
 
     _save_button: Button
+    _reset_button: Button
+
     _error_log: Static
 
     def __init__(self):
@@ -27,18 +31,24 @@ class ConfigWindow(Window):
         # Define save button
         self._save_button: Button = Button("Save", classes="FormButton")
 
-        # Define header
-        header: Static = Static(AppConstants.CONFIG_LOGO, classes="FormHeader")
+        # Define reset button
+        self._reset_button: Button = Button("Reset", classes="FormButton")
 
-        # Gap between form and button
+
+        # Define header
+        self._header: Static = Static(AppConstants.CONFIG_LOGO, classes="FormHeader")
+
+        # Gaps (TO DO: Find a less hacky way to render these)
         gap = Static("")
         gap2 = Static("")
+        gap3 = Static("")
+        gap4 = Static("")
 
         # Define error log
         self._error_log: Static =  Static("", classes="FormError")
 
         # These are the widgets that will load within the window
-        widgets: list[Widget] = [header, self._form, gap, Center(self._save_button), gap2, self._error_log]
+        widgets: list[Widget] = [self._header, self._form, gap, gap2, Center(self._save_button), gap3, Center(self._reset_button), gap4, self._error_log]
 
         super().__init__(widgets)
 
@@ -49,6 +59,10 @@ class ConfigWindow(Window):
             result: ConfigOperationResult = ConfigParser.save_config(ConfigConstants.CONFIG_FILE_PATH, self._config_inputs)
 
             self._log_result(result)
+
+        # Reset form (Re-load JSON)
+        elif event.button == self._reset_button:
+            self.load_json()
 
     def load_json(self):
 
